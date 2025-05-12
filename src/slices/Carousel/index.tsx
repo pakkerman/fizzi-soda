10aa46f794743577e3766dbc9a101a961482c30b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-12 16:46:26 +0800

    feat: change flavors

--- src/slices/Carousel/index.tsx
+++ src/slices/Carousel/index.tsx
@@ -1,6 +1,33 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+
+import { Bounded } from "@/components/Bounded";
+import { SodaCanProps } from "@/components/SodaCan";
+import { Center, Environment, View } from "@react-three/drei";
+import FloatingCan from "@/components/FloatingCan";
+
+const FLAVORS: {
+  flavor: SodaCanProps["flavor"];
+  color: string;
+  name: string;
+}[] = [
+  { flavor: "blackCherry", color: "#710523", name: "Black Cherry" },
+  { flavor: "grape", color: "#572981", name: "Grape Goodness" },
+  { flavor: "lemonLime", color: "#164405", name: "Lemon Lime" },
+  {
+    flavor: "strawberryLemonade",
+    color: "#690B3D",
+    name: "Strawberry Lemonade",
+  },
+  { flavor: "watermelon", color: "#4B7002", name: "Watermelon Crush" },
+];
 
 /**
  * Props for `Carousel`.
@@ -9,15 +36,64 @@ export type CarouselProps = SliceComponentProps<Content.CarouselSlice>;
 
 /**
  * Component for "Carousel" Slices.
+ *
  */
+
 const Carousel: FC<CarouselProps> = ({ slice }) => {
+  const [currentFlavorIndex, setCurrentFlavorIndex] = useState(0);
+
+  function changeFlavor(idx: number) {
+    const nextIdx = (idx + FLAVORS.length) % FLAVORS.length;
+    setCurrentFlavorIndex(nextIdx);
+  }
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="carousel relative grid h-screen grid-rows-[auto,4fr,auto] justify-center overflow-hidden bg-white py-12 text-white"
     >
-      Placeholder component for carousel (variation: {slice.variation}) Slices
-    </section>
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50 " />
+
+      <h2 className="relative text-center text-5xl font-bold">
+        <PrismicText field={slice.primary.heading} />
+      </h2>
+
+      <div className="grid grid-cols-[auto,auto,auto] items-center">
+        {/* Left */}
+        <button
+          onClick={() => changeFlavor(currentFlavorIndex + 1)}
+          className="z-20"
+        >
+          Left
+        </button>
+        {/* Can */}
+        <View className="aspect-square h-[70vmin]">
+          <Center position={[0, 0, 1.5]}>
+            <FloatingCan
+              floatIntensity={0.3}
+              rotationIntensity={1}
+              flavor={FLAVORS[currentFlavorIndex].flavor}
+            />
+          </Center>
+          <Environment
+            files="/hdr/lobby.hdr"
+            environmentIntensity={0.6}
+            environmentRotation={[0, 3, 0]}
+          />
+          <directionalLight intensity={6} />
+        </View>
+        {/* Right */}
+
+        <button
+          onClick={() => changeFlavor(currentFlavorIndex - 1)}
+          className="z-20"
+        >
+          Right
+        </button>
+        <PrismicRichText field={slice.primary.price_copy} />
+      </div>
+    </Bounded>
   );
 };
 
