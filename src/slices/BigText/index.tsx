69ed392fc2fc7bd74d49f828c9e0e6c932b830b8
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-14 11:42:48 +0800

    feat: footer

--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -15,9 +15,9 @@ const BigText: FC<BigTextProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="flex min-h-screen w-screen flex-col items-center justify-center overflow-hidden bg-[#FE6334] text-[#FEE832]"
+      className=" flex items-center min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]"
     >
-      <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[0.7] ">
+      <h2 className="grid w-full gap-[3vw]  py-10 text-center font-black uppercase leading-[0.7] ">
         <div className="text-[34vw]">Soda</div>
         <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
           <span className="inline-block">that</span>
