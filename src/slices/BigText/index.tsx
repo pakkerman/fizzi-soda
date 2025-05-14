efbf1a42f801f0b5345dd0ebbfda1188ccf7e091
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-05-14 11:16:39 +0800

    feat: big text content

--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -15,8 +15,17 @@ const BigText: FC<BigTextProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="flex min-h-screen w-screen flex-col items-center justify-center overflow-hidden bg-[#FE6334] text-[#FEE832]"
     >
-      Placeholder component for big_text (variation: {slice.variation}) Slices
+      <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[0.7] ">
+        <div className="text-[34vw]">Soda</div>
+        <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
+          <span className="inline-block">that</span>
+          <span className="inline-block max-md:text-[27vw]">makes</span>
+          <span className="inline-block max-md:text-[40vw]">you</span>
+        </div>
+        <div className="text-[32vw]">Smile</div>
+      </h2>
     </section>
   );
 };
